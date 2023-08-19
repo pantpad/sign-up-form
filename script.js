a7a1078f418ba461bdbf632e56df74a5d63bd173
pantpad <138708449+pantpad@users.noreply.github.com>
2023-08-19 14:57:45 +0200

    Add password confirmation validation using JS
    Implemented by chaning the pattern on the confirm password
    It will be set to be exactly as the password inputed if correct
    
    formatRegex is a simple function to check for password that
    end with the '$' symbol.

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("wela");
+let     pswSet = false; 
+const   confirmPassword = document.getElementById("password_confirm");
+const   password = document.getElementById("password");
+
+password.addEventListener("input",(e) => { 
+    if(e.target.value == "" || e.target.value == null || e.target.value == undefined)
+        confirmPassword.setAttribute("pattern","");
+    else{
+        if(password.validity.valid){
+            console.log("password valida: " + e.target.value);
+            confirmPassword.pattern = formatRegex(e.target.value);
+            console.log(formatRegex(e.target.value));
+        }
+    }
+});
+
+function formatRegex(input){
+    return input.replaceAll("$", "\\$");
+}
