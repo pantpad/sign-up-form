45129538a9a6e4c70a8638d52a9a3164d8b15421
pantpad <138708449+pantpad@users.noreply.github.com>
2023-08-20 19:56:43 +0200

    Implement password criteria color changes

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ console.log("wela");
 let     pswSet = false; 
 const   confirmPassword = document.getElementById("pswConfirm");
 const   password = document.getElementById("password");
+const   criteriaContainer = document.querySelectorAll('.criteria p');
+
+const   criteriaList = [
+    /\d/,        //8 chars
+    /.{8,}/,        //1 uppercase letter
+    /[A-Z]/,           //1 digit
+    /[^a-zA-Z\d]/   //1 special character
+]
 
 password.addEventListener("input",(e) => { 
     if(e.target.value == "" || e.target.value == null || e.target.value == undefined)
@@ -11,6 +19,12 @@ password.addEventListener("input",(e) => {
             console.log("password valida: " + e.target.value);
             confirmPassword.pattern = formatRegex(e.target.value);
             console.log(formatRegex(e.target.value));
+        }else{
+            criteriaList.forEach((criteria,index) => {
+                if(criteria.test(e.target.value)){
+                    criteriaContainer[index].style.color = "green";
+                }
+            });
         }
     }
 });
